64e11e03789e726b4d2ef8309e8fedbafcd482fa
Sukhdip Rai <sukhdipsrai@gmail.com>
2020-12-17 01:29:27 -0500

    phase 2 study and refined

--- skeleton/phase_2_enumerable.js
+++ skeleton/phase_2_enumerable.js
@@ -1,11 +1,22 @@
 Array.prototype.myEach = function(callback){
     for(let i = 0; i < this.length; i++){
-        this[i] = callback(this[i])
+        callback(this[i])
     }
 };
 
-Array.prototype.myEach = function(callback){
-    for(let i = 0; i < this.length; i++){
-        this[i] = callback(this[i]);
+Array.prototype.myMap = function(callback){
+    const ans = []
+
+    this.myEach(ele => ans.push(callback(ele)));
+    return ans;
+};
+
+Array.prototype.myReduce = function(callback, acc){
+    if (typeof initialValue === undefined){
+        initialValue = arr[0];
+        arr = arr.slice(1);
     }
+
+    this.myEach(ele => acc = callback(acc,ele));
+    return acc;
 };
